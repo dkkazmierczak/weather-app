43a1ac4b27677dc0e6fc3455de9e73d819173894
Dominika Kaźmierczak <dkl.kazmierczak@gmail.com>
2022-01-09 21:24:19 +0100

    Fixed to 7 day forecast

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ function displayDailyForecast(response) {
   let forecastHTML = `<table class="table table-sm shadow">`
 
   forecast.forEach(function (forecastDay, index) {
-    if (index > 0 && index < 7) {
+    if (index > 0 && index < 8) {
       forecastHTML =
         forecastHTML + ` <tbody>
           <tr>
